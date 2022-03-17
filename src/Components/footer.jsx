e12215c74120d0ccdf4a3c98f20456a60c5cd523
Frank <codingfrank73@gmail.com>
2022-03-17 21:42:44 +0100

    fix more netlify problems

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -12,13 +12,13 @@ const Footer = () => {
                     </div>
                     <div className="mitte">
                         <div>
-                            <a href="https://www.instagram.com/" target="_blank" rel='noopener'><i className="fa-brands fa-instagram"></i></a>
-                            <a href="https://www.facebook.com/" target="_blank" rel='noopener'><i className="fa-brands fa-facebook-square"></i></a>
+                            <a href="https://www.instagram.com/" target="_blank" rel='noreferrer'><i className="fa-brands fa-instagram"></i></a>
+                            <a href="https://www.facebook.com/" target="_blank" rel='noreferrer'><i className="fa-brands fa-facebook-square"></i></a>
                         </div>
                     </div>
                     <div className="rechts">
                         <a href="https://de.wikipedia.org/wiki/Make_love,_not_war#/media/Datei:Love_war_(6405241535).jpg"
-                            target="_blank" rel='noopener'>
+                            target="_blank" rel='noreferrer'>
                             <p><i className="fa-solid fa-heart"></i> MAKE LOVE</p>
                             <p>NOT WAR <i className="fa-solid fa-peace"></i></p>
                         </a>
