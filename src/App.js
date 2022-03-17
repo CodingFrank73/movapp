8820e8613b2e290ab63172caae57634babfaf9b3
Frank <codingfrank73@gmail.com>
2022-03-17 21:04:08 +0100

    del referenz in App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@
 // import './App.css';
 import './Styles/main.css'
 import Home from './Components/home';
-import Header from './Components/header'
-import Footer from './Components/footer'
+
 
 function App() {
   return (
